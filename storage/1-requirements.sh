1b0f6c8874f5fda8e341e044d5d37347e8fe831a
jproque-dell <jeanpierre.roquesalane@dell.com>
2021-03-15 03:58:35 -0500

    initial commit

---
+++ storage/1-requirements.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+oc create namespace openshift-local-storage
+oc label namespace openshift-local-storage "openft.io/cluster-monitoring=true"
+
+# Go to UI and install Local Storage Operator
+
+# Verify nodes are labeled correctly
+# Should display workers only
+oc get nodes -l cluster.ocs.openshift.io/openshift-storage
+
+# Deploy disk-id DaemonSet
+oc apply -f https://gist.githubusercontent.com/jproque-dell/f030ca96e01ff54dc88b4b33cbda3653/raw/61feb52d0230a95e0d284c650155c9f3ae159e5f/disk-id-ds.yaml
+oc get pods -l name=disk-gatherer -n default
+kubectl logs -n default -l name=disk-gatherer
+# Gather the disk IDs and fill in the 2-local-block-storage.yaml with these values
